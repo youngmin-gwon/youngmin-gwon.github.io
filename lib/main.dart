b878f727682d3cc8f08a11f0bcdd6209efb845e7
Youngmin Gwon <youngmin-gwon@users.noreply.github.com>
2023-06-20 23:21:57 +0900

    💄 style(typo): add typography
    - add pyeonchang, gmarket sans typography for assets
    - add reference page to check how it looks like

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/core/presentation/theme/theme.dart';
+import 'package:portfolio/core/presentation/theme/type_example_widget.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +11,12 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      theme: MyTheme.light,
+      darkTheme: MyTheme.dark,
+      themeMode: ThemeMode.system,
+      themeAnimationCurve: Curves.easeOut,
+      home: const TypographyExampleWidget(),
     );
   }
 }
